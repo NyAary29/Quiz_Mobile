4799ae07b3efcc2bea1846ff11fc6cb575796836
heriniavotra <heriniavotratoky@gmail.com>
2024-08-01 15:01:18 +0300

    final login

--- app/src/main/java/com/example/quiz_game/Login.kt
+++ app/src/main/java/com/example/quiz_game/Login.kt
@@ -1,8 +1,14 @@
 package com.example.quiz_game
 
-
-
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,13 +20,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,49 +63,113 @@ fun LoginForm(modifier: Modifier = Modifier){
     var name = "toky"
     var email = "wertyu@hgf.com"
     var phone = "034 81 107 32"
-    Column(
+
+    Card (
         modifier = Modifier
+            .fillMaxWidth()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "WELCOME",
-            fontSize = 30.sp,
-            fontFamily = FontFamily.Serif,
-            color = Color.Red,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
 
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Nom") },
+        elevation = CardDefaults.elevatedCardElevation(10.dp)
+    ){
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
+                .background(color = Color.White)
+                .padding(bottom = 30.dp),
 
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        OutlinedTextField(
-            value = phone,
-            onValueChange = { phone = it },
-            label = { Text("Phone") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-        )
+            val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+
+            val colors = listOf(
+                Color(0xFFFFA500),  // Orange
+                Color(0xFF90EE90), //vert
+                Color(0xFFFFEB3B), // Jaune
+                Color(0xFF4285F4), // Bleu
+                Color(0xFF808080), // Gris
+
+            )
+            val scale by infiniteTransition.animateFloat(
+                initialValue = 0.8f,
+                targetValue = 1.5f,
+                animationSpec = infiniteRepeatable(tween(1200), RepeatMode.Reverse),
+                label = "scale"
+            )
+
+            val animatedColor by infiniteTransition.animateColor(
+                initialValue = colors[0],
+                targetValue = colors[1],
+                animationSpec = infiniteRepeatable(
+                    animation = keyframes {
+                        this.durationMillis = 4000
+                        colors[1] at 1000
+                        colors[2] at 2000
+                        colors[3] at 3000
+                        colors[0] at 4000
+                    },
+                    repeatMode = RepeatMode.Restart
+                ),
+                label = "color"
+            )
+            Text(
+                text = "WELCOME",
+                fontSize = 30.sp,
+                fontFamily = FontFamily.Serif,
+                color = animatedColor,
+                modifier = Modifier
+                    .padding(bottom = 10.dp)
+                    .padding(top = 30.dp)
+                    .graphicsLayer {
+                        scaleX = scale
+                        scaleY = scale
+                        transformOrigin = TransformOrigin.Center
+                    }
+                    .align(Alignment.CenterHorizontally),
+            )
+
+            Text(
+                text ="Entrez vos informations pour commencer le quiz",
+                fontSize = 11.sp,
+                fontFamily = FontFamily.Serif,
+                color = Color.Black,
+                modifier = Modifier.padding(bottom = 30.dp)
+            )
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Nom") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .padding(bottom = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .padding(bottom = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = phone,
+                onValueChange = { phone = it },
+                label = { Text("Phone") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .padding(bottom = 10.dp)
+            )
+        }
     }
+
 }
 
 @Composable
